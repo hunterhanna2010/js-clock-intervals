ee965fd5d54196ad3147bb708a26d80d76074182
hunterhann2010 <josh.m.hunter@gmail.com>
2019-07-30 16:06:20 -0700

    clock project

--- clock.js
+++ clock.js
@@ -1,9 +1,9 @@
 let start = 'Hello World';
 console.log(start);
 //define variables that hold the degrees of rotation
-var degreesInSeconds = 6;
-var degreesInMinutes = 200;
-var degreesInHours = 150;
+var degreesInSeconds = 0;
+var degreesInMinutes = 0;
+var degreesInHours = 0;
 //define variables that access the html Id elements
 var secondsHand = document.getElementById("second");
 var minutesHand = document.getElementById("minute");
@@ -14,41 +14,26 @@ var hourHand = document.getElementById("hour");
 
 
 setInterval(function() {
-	secondsHand.style.transform = "rotate(" + degreesInSeconds + "deg)";
+	secondsHand.style.transform = "rotate(" + (degreesInSeconds/60)*360 + "deg)";
 	degreesInSeconds ++;
     console.log("TICK!");
  }, 1000);
 
-setInterval(function() {
-	minutesHand.style.transform = "rotate(" + degreesInMinutes + "deg)";
-	degreesInMinutes ++;
-	console.log('minutes');
-}, 1000);
+ setInterval(function() {
+ 	minutesHand.style.transform = "rotate(" + (degreesInMinutes/60)*360 + "deg)";
+ 	degreesInMinutes ++;
+ 	console.log('minutes');
+ }, 30000);
 
-setInterval(function() {
-	hourHand.style.transform = "rotate(" + degreesInHours + "deg)";
+ setInterval(function() {
+ 	hourHand.style.transform = "rotate(" + (degreesInHours/12)*360 + "deg)";
 	degreesInHours ++;
-	console.log('Hours');
-}, 1000);
+ 	console.log('Hours');
+ }, 36000000);
 
-	
 
-//define functions that convert seconds, minutes and hours to how many degrees the hand should be rotated around the clock!
-function secondsToMinutes(time){
-	return Math.floor(time / 60)
-}
-secondRotation(0); // returns 0
-secondRotation(59); // returns (59 / 60) * 360
-
-hourRotation(12); // returns 0
-hourRotation(5); // returns (5 / 12) * 360
-
-//The first argument is a callback function - it gets triggered every 1000 milliseconds (1 second), which is the second argument.
-
-setInterval(functionYouWantToRun, millisecondsDelay);
-
-//function annoy() {
-// // console.log('Are we there yet?');
+//var updateClockToCurrent = () => {
+	//secondsHand(now.getSeconds())
+	//minutesHand(now.getMinutes())
+	//hourHand(now.getHours())
 //}
-
-//setInterval(annoy, 1000);
